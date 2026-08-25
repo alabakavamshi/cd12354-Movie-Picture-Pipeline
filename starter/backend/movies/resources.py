2978438b9bd52457b1a8cbbb5397c5eda588dc80
Vamshi Krishna <YOUR_GITHUB_EMAIL>
2026-08-25 05:04:39 +0000

    Fix backend CRUD and upgrade EKS

--- starter/backend/movies/resources.py
+++ starter/backend/movies/resources.py
@@ -1,4 +1,4 @@
-from flask import jsonify
+from flask import jsonify, request
 from flask.views import MethodView
 
 # Dummy database to hold movie examples
@@ -10,10 +10,95 @@ movies = {
 
 
 class Movies(MethodView):
+
     def get(self, movie_id):
         if movie_id is None:
-            # Return a list of all movies
-            return jsonify({"movies": [dict({"title": movie["title"]}, **{"id": i}) for i, movie in movies.items()]})
-        else:
-            # Return the details of a specific movie
-            return jsonify({"movie": movies[str(movie_id)]})
+            return jsonify({
+                "movies": [
+                    {
+                        "id": movie_id,
+                        "title": movie["title"],
+                    }
+                    for movie_id, movie in movies.items()
+                ]
+            })
+
+        movie = movies.get(str(movie_id))
+
+        if movie is None:
+            return jsonify({"error": "Movie not found"}), 404
+
+        return jsonify({
+            "movie": {
+                "id": str(movie_id),
+                **movie
+            }
+        })
+
+    def post(self):
+        data = request.get_json(silent=True)
+
+        if not data:
+            return jsonify({"error": "JSON body is required"}), 400
+
+        title = data.get("title")
+        description = data.get("description")
+
+        if not title or not description:
+            return jsonify({
+                "error": "title and description are required"
+            }), 400
+
+        movie_id = str(max([int(i) for i in movies.keys()] or [0]) + 1)
+
+        movies[movie_id] = {
+            "title": title,
+            "description": description,
+        }
+
+        return jsonify({
+            "movie": {
+                "id": movie_id,
+                **movies[movie_id]
+            }
+        }), 201
+
+    def put(self, movie_id):
+        movie_id = str(movie_id)
+
+        if movie_id not in movies:
+            return jsonify({"error": "Movie not found"}), 404
+
+        data = request.get_json(silent=True)
+
+        if not data:
+            return jsonify({"error": "JSON body is required"}), 400
+
+        if "title" in data:
+            movies[movie_id]["title"] = data["title"]
+
+        if "description" in data:
+            movies[movie_id]["description"] = data["description"]
+
+        return jsonify({
+            "movie": {
+                "id": movie_id,
+                **movies[movie_id]
+            }
+        })
+
+    def delete(self, movie_id):
+        movie_id = str(movie_id)
+
+        if movie_id not in movies:
+            return jsonify({"error": "Movie not found"}), 404
+
+        deleted_movie = movies.pop(movie_id)
+
+        return jsonify({
+            "message": "Movie deleted",
+            "movie": {
+                "id": movie_id,
+                **deleted_movie
+            }
+        })
